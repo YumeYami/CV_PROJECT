1a65ac8aa60d1cbf0847922865d25fc34f7e35d1
yumeyami <iscream.p@gmail.com>
2014-11-09 18:46:36 +0700

    add personID class

---
+++ CV_PROJECT/personID.cpp
@@ -0,0 +1,10 @@
+#include "personID.h"
+
+int personID::serial = 0;
+
+personID::personID() {
+}
+
+
+personID::~personID() {
+}
---
+++ CV_PROJECT/personID.h
@@ -0,0 +1,8 @@
+#pragma once
+class personID {
+public:
+	static int serial;
+	personID();
+	~personID();
+};
+
